c4ad8e7fe6488adb21f4c2825563db9073ed6c7a
Kai Waelti <kai.waelti@dfki.de>
2019-09-24 11:11:11 +0200

    Add grip and xdotool

--- install.arch.sh
+++ install.arch.sh
@@ -21,12 +21,26 @@ sudo npm install -g neovim
 echo_info "Installing Linters"
 sudo npm -g install jshint jsxhint jsonlint stylelint sass-lint
 sudo npm -g install raml-cop markdownlint-cli write-good
-pip install --user pycodestyle pyflakes flake8 vim-vint proselint yamllint
+pip install --user pycodestyle pyflakes flake8 vim-vint proselint yamllint grip
 sudo pacman -S shellcheck tidy
+
+echo_info "Installing dependencies from sources"
+mkdir -p deps && cd deps || exit
 
 echo_info "Patch existing fonts via installing powerline font collection"
 git clone git@github.com:powerline/fonts.git
 cd fonts || return
+./install.sh
+cd ..
+
+echo_info "Installing xdotool for fake keyboard/mouse input, WM and more"
+git clone git@github.com:jordansissel/xdotool.git
+cd xdotool || return
+make
+sudo make install
+cd ..
+
+echo_done "Everything was installed successfully"
 
 nvim -c checkhealth
 
